08456e34a79806783fbb6b6c12c458df3a6edd0a
Binwant Singh Sahota <sahotab@uwindsor.ca>
2024-02-22 00:05:11 -0500

    created views(register,login, logout), created templates sign-in.html, sign-up.html

--- bankingApp/urls.py
+++ bankingApp/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path("about/",views.about,name="about"),
     path("contact/",views.contact,name="contact"),
     path("", include("core.urls")),
-    path("user/", include("userauths.urls"))
+    path("user/", include("userauths.urls")),
+
     ]
--- userauths/views.py
+++ userauths/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from userauths.forms import UserRegisterForm
-from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from userauths.models import User
+from userauths.forms import UserRegisterForm
+
 
 def RegisterView(request):
+    # if request.user.is_authenticated:
+    #     messages.warning(request, f"You are already logged in.")
+    #     return redirect("account:account")
+
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -20,13 +25,41 @@ def RegisterView(request):
             login(request, new_user)
             return redirect("core:index")
 
-    if request.user.is_authenticated:
-        messages.warning(request, f"You are already logged in.")
-        return  redirect("core:index")
-
     else:
         form = UserRegisterForm()
     context = {
         "form": form
     }
     return render(request, "userauths/sign-up.html", context)
+
+
+def LoginView(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:  # if there is a user
+                login(request, user)
+                messages.success(request, "You are logged.")
+                return redirect("core:index")
+            else:
+                messages.warning(request, "Username or password does not exist")
+                return redirect("userauths:sign-in")
+        except:
+            messages.warning(request, "User does not exist")
+
+    # if request.user.is_authenticated:
+    #     messages.warning(request, "You are already logged In")
+    #     return redirect("account:account")
+
+    return render(request, "userauths/sign-in.html")
+
+
+def LogoutView(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect("userauths:sign-in")
