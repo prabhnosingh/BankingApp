aca25e5af637321c3849f41bb95d1bb7b3b0654c
RajdeepToor <toor81@uwindsor.ca>
2024-02-26 01:43:59 -0500

    KYCform widgets addition

--- account/forms.py
+++ account/forms.py
@@ -7,3 +7,23 @@ class DateInput(forms.DateInput):
 
 class KYCForm(forms.ModelForm):
     identity_image = ImageField(widget=FileInput)
+
+    class Meta:
+        model = KYC
+        fields=['full_name','image','marrital_status','identity_type','date_of_birth','signature','city','mobile','fax','state','country','gender','identity_image']
+
+    widgets={
+        "full_name":forms.TextInput(attrs=
+                                    {'placeholder':'Full Name'}
+                                    ),
+        "mobile": forms.TextInput(attrs=
+                                  {'placeholder': 'Mobile'}
+                                  ),
+        "fax": forms.TextInput(attrs=
+                               {'placeholder': 'Fax number'}
+                               ),
+
+        "country": forms.TextInput(attrs= {'placeholder': 'Country'}),
+        "state": forms.TextInput(attrs={'placeholder': 'State'}),
+        "city": forms.TextInput(attrs={'placeholder': 'City'}),
+    }
