6dc037ee91394957486e4258d75e1b8eee5b2574
RajdeepToor <toor81@uwindsor.ca>
2024-02-26 21:28:19 -0500

    url updates in account app

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,5 @@ app_name = "account"
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
     path("", views.account, name="account"),
-    path("kyc-reg/", views.kyc_registration, name="kyc-reg"),
+    path("kyc-registration/", views.kyc_registration, name="kyc-reg"),
 ]
