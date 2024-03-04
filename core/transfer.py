c901f1bd3acb0c9691f1b56cc79b0c1276190dd0
Avinoorsingh <avinoorsingh123@gmail.com>
2024-03-03 21:45:13 -0500

    seach-user-by-acc-no html page created

--- core/transfer.py
+++ core/transfer.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect
 from account.models import  Account
 from django.contrib.auth.decorators import  login_required
 
-@login_required
+# @login_required
 def search_users_by_account_number(request):
     return render(request, "transfer/search-user-by-account-number.html")
