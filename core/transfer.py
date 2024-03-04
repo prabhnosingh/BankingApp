105729785e5a2edaeb368f2a884400c5fabd4b13
Avinoorsingh <avinoorsingh123@gmail.com>
2024-03-03 20:03:57 -0500

    created transfer.py

--- core/transfer.py
+++ core/transfer.py
@@ -1,1 +1,7 @@
-from account.models import  Account;
+from django.shortcuts import render, redirect
+from account.models import  Account
+from django.contrib.auth.decorators import  login_required
+
+@login_required
+def search_users_by_account_number(request):
+    return render(request, "transfer/search-user-by-account-number.html")
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from core import views
+from core import transfer
 
 app_name = 'core'
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+
+    #Transfers
+    path("search-account/", transfer.search_users_by_account_number, name="search-account")
+
+    #request money
+
+
+    #add debit card
 ]
