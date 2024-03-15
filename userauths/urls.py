10bbe6d9ffaa0857291ff876ac9a9055a523759a
RajdeepToor <toor81@uwindsor.ca>
2024-03-15 19:16:45 -0400

    Added forgot password (working on logic )and ammended styling plus refactoring of assets files

--- userauths/urls.py
+++ userauths/urls.py
@@ -6,5 +6,6 @@ app_name = 'userauths'
 urlpatterns = [
     path("sign-up/", views.RegisterView, name="sign-up"),
     path("sign-in/", views.LoginView, name="sign-in"),
-    path("sign-out/", views.logoutView, name="sign-out")
+    path("sign-out/", views.logoutView, name="sign-out"),
+    path("password_reset_confirm/", views.password_reset_confirm, name="password_reset_confirm")
 ]
