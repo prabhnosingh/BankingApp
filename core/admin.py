d36b589a9a4e5f2385bf8551477b8f3eec1ef2d7
RajdeepToor <toor81@uwindsor.ca>
2024-03-20 22:28:39 -0400

    Created Model for credit card and registered adminModel

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from core.models import Transaction
+from core.models import Transaction,CreditCard
 
 class TransactionAdmin(admin.ModelAdmin):
     list_editable = ['amount', 'status', 'transaction_type', 'reciever', 'sender']
     list_display = ['user', 'amount', 'status', 'transaction_type', 'reciever', 'sender']
 
+class CreditCardAdmin(admin.ModelAdmin):
+    list_editable = ['amount', 'card_type']
+    list_display = ['user', 'amount', 'card_type']
+
 # Register your models here.
 admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(CreditCard,CreditCardAdmin)
