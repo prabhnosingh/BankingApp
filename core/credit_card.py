266307221a34d8c11df71dd38d3d7c7a965140c5
prabh <prabhnoorsingh23000@gmail.com>
2024-03-22 01:28:58 -0400

    Updating the fund and delete credit card functionality

--- core/credit_card.py
+++ core/credit_card.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect
 from account.models import KYC, Account
 from core.models import Transaction
@@ -5,6 +6,7 @@ from account.forms import KYCForm
 from django.contrib import messages
 from core.forms import CreditCardForm
 from core.models import CreditCard
+from decimal import Decimal
 
 
 def card_detail(request, card_id):
@@ -16,6 +18,34 @@ def card_detail(request, card_id):
         "credic_card": credic_card,
     }
     return render(request, "credit_card/card-detail.html", context)
+
+
+def delete_card(request, card_id):
+    credit_card = CreditCard.objects.get(card_id=card_id, user=request.user)
+
+    # New Feature
+    # BEfore deleting card, it'll be nice to transfer all the money from the card to the main account balance.
+    account = request.user.account
+
+    if credit_card.amount > 0:
+        account.account_balance += credit_card.amount
+        account.save()
+
+        # Notification.objects.create(
+        #     user=request.user,
+        #     notification_type="Deleted Credit Card"
+        # )
+
+        credit_card.delete()
+        messages.success(request, "Card Deleted Successfull")
+        return redirect("account:dashboard")
+    Notification.objects.create(
+        user=request.user,
+        notification_type="Deleted Credit Card"
+    )
+    credit_card.delete()
+    messages.success(request, "Card Deleted Successfull")
+    return redirect("account:dashboard")
 
 
 def fund_credit_card(request, card_id):
@@ -32,11 +62,11 @@ def fund_credit_card(request, card_id):
             credit_card.amount += Decimal(amount)
             credit_card.save()
 
-            Notification.objects.create(
-                amount=amount,
-                user=request.user,
-                notification_type="Funded Credit Card"
-            )
+            # Notification.objects.create(
+            #     amount=amount,
+            #     user=request.user,
+            #     notification_type="Funded Credit Card"
+            # )
 
             messages.success(request, "Funding Successfull")
             return redirect("core:card-detail", credit_card.card_id)
